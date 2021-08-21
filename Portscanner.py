217a7d440d1dbac9f933235f4bdb644e06c3c7d1
Terraminator <49759824+Terraminator@users.noreply.github.com>
2021-08-21 20:14:15 +0200

    Add files via upload

--- Portscanner.py
+++ Portscanner.py
@@ -5,21 +5,23 @@ from time import sleep
 from datetime import datetime
 import os
 
-
 content = ""
 path = os.getcwd() + "\\ips.txt"
-print(path)
 if not os.path.exists(path):
 	with open("ips.txt", "w") as f:
 		f.write("")
 f = open("ips.txt", "r")
 targets = f.readlines()
 f.close()
-#targetIP = socket.gethostbyname(target)
-#targetIP = target
+if len(targets) < 1:
+	print("Please write at least one ip in ips.txt!")
+	input("PRESS ANY KEY TO FINISH")
+	sys.exit()
+
 with open("ports.txt", "w") as f:
 	f.write("")
 tstart = datetime.now()
+
 def scanner(y):
 	content = ""
 	id = threading.currentThread().getName()
@@ -39,8 +41,10 @@ for ip in targets:
 		f.write(ip)
 		f.write("\n")
 	q = 1
+	if "www" in ip or "org" in ip or "com" in ip or "de" in ip or "en" in ip or "us" in ip or "ch" in ip:
+		ip = socket.gethostbyname(ip)
 	targetIP = ip
-	print(ip)
+	print(ip + "\n")
 	while q < 4501:
 		try:
 			x = [q]
@@ -58,5 +62,12 @@ print("Scan completed in " + str(diff))
 datei = open("ports.txt","r")
 oports = datei.read()
 datei.close()
-print("Open Ports: ")
-print(oports)
+if oports != "":
+	print("Open Ports: ")
+	print(oports)
+	input("PRESS ANY KEY TO FINISH")
+	sys.exit()
+elif oports == "":
+	print("No Ports are opened!")
+	input("PRESS ANY KEY TO FINISH")
+	sys.exit()
