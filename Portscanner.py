238c800740096f43c28108c2e45b4b0730c6fc95
Terraminator <49759824+Terraminator@users.noreply.github.com>
2021-08-21 20:54:28 +0200

    Add files via upload

--- Portscanner.py
+++ Portscanner.py
@@ -23,38 +23,42 @@ with open("ports.txt", "w") as f:
 tstart = datetime.now()
 
 def scanner(y):
-	content = ""
-	id = threading.currentThread().getName()
-	p = y[0]
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	res = sock.connect_ex((targetIP, p))
-	if res == 0:
-		print("Open Connection in Port: " + str(p))
-		datei = open("ports.txt","a+")
-		datei.write(str(p) + ", ")
-		datei.close()
-	sock.close()
+	try:
+		content = ""
+		id = threading.currentThread().getName()
+		p = y[0]
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		res = sock.connect_ex((targetIP, p))
+		if res == 0:
+			print("Open Connection in Port: " + str(p))
+			datei = open("ports.txt","a+")
+			datei.write(str(p) + ", ")
+			datei.close()
+		sock.close()
+	except:
+		pass
 
 for ip in targets:
-	with open("ports.txt", "a+") as f:
-		f.write("\n")
-		f.write(ip)
-		f.write("\n")
-	q = 1
-	if "www" in ip or "org" in ip or "com" in ip or "de" in ip or "en" in ip or "us" in ip or "ch" in ip:
-		ip = socket.gethostbyname(ip)
-	targetIP = ip
-	print(ip + "\n")
-	while q < 4501:
-		try:
-			x = [q]
-			t = threading.Thread(target=scanner, args=[x])
-			t.start()
-			q = q + 1
-		except:
-			sleep(0.5)
-			q = q - 1
-	
+	if ip != "":
+		with open("ports.txt", "a+") as f:
+			f.write("\n")
+			f.write(ip)
+			f.write("\n")
+		q = 1
+		if "www" in ip or "org" in ip or "com" in ip or "de" in ip or "en" in ip or "us" in ip or "ch" in ip:
+			ip = socket.gethostbyname(ip)
+		targetIP = ip
+		print(ip + "\n")
+		while q < 4501:
+			try:
+				x = [q]
+				t = threading.Thread(target=scanner, args=[x])
+				t.start()
+				q = q + 1
+			except:
+				sleep(0.5)
+				q = q - 1
+
 
 tend = datetime.now()
 diff = tend - tstart
@@ -65,9 +69,9 @@ datei.close()
 if oports != "":
 	print("Open Ports: ")
 	print(oports)
-	input("PRESS ANY KEY TO FINISH")
+	input("PRESS ENTER TO FINISH")
 	sys.exit()
 elif oports == "":
 	print("No Ports are opened!")
-	input("PRESS ANY KEY TO FINISH")
+	input("PRESS ENTER TO FINISH")
 	sys.exit()
